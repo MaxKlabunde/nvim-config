357b735c1eaa61be9f9eaeb16fab920b29c25188
MaxKlabunde <ma871kla@htwg-konstanz.de>
2026-05-22 12:55:32 +0200

    better split navigation, clipboard support and window size adjustment

--- lua/custom/plugins/vim-tmux-navigator.lua
+++ lua/custom/plugins/vim-tmux-navigator.lua
@@ -1,5 +1,9 @@
 return {
   "christoomey/vim-tmux-navigator",
+  init = function()
+    ---@diagnostic disable-next-line: undefined-global
+    vim.g.tmux_navigator_no_wrap = 1
+  end,
   cmd = {
     "TmuxNavigateLeft",
     "TmuxNavigateDown",
